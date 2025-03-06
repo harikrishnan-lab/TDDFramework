c83c8cf4932b3bb28b45fc365a2e92a0d6e4e73d
Hari.Krishnan <k5bhzbzag64ymr235bc3py5e2wlxeuqszujb3bakn7xhi4ezz47q>
2025-03-06 13:05:24 +0530

    browser close

--- DemoApplication/src/test/java/org/example/AppTest.java
+++ DemoApplication/src/test/java/org/example/AppTest.java
@@ -19,5 +19,6 @@ public class AppTest
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
         driver.findElement(By.id("login-button")).click();
+        driver.quit();
     }
 }
