f6be257552dfae19520708ea20300c7c3c08efb7
Hari.Krishnan <k5bhzbzag64ymr235bc3py5e2wlxeuqszujb3bakn7xhi4ezz47q>
2025-03-06 12:57:23 +0530

    initial commit

---
+++ DemoApplication/src/test/java/org/example/AppTest.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/v1/");
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+    }
+}
